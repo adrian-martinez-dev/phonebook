c2fc35ec45b14a3a48193961e90b59093d8c4345
Adrian Martinez <n.bicalcarata@gmail.com>
2018-04-29 11:09:36 -0500

    Fix style and center

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,12 @@
         <title>Agenda telefónica</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
-        <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+                font-family: 'Quicksand', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
@@ -65,25 +63,27 @@
         </style>
     </head>
     <body>
-        <div class="position-ref full-height">
+      <div class="position-ref full-height">
+        <div class="content">
 
-            <div class="content">
-                <div>
-                  <h1>
-                    Agenda Telefónica
-                  </h1>
-                  <div class="col-lg-6">
-                    <div class="input-group">
-                      <input type="text" class="form-control" placeholder="Contacto...">
-                      <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">Buscar</button>
-                      </span>
-                    </div><!-- /input-group -->
-                  </div><!-- /.col-lg-6 -->
+          <h1>
+            Agenda Telefónica
+          </h1>
+
+              <div class="mx-auto" style="width: 400px;">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Contacto...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">Buscar</button>
+                  </span>
                 </div>
+              </div>
 
-               </div>
-            </div>
         </div>
+
+<!-- JQuery -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     </body>
 </html>
