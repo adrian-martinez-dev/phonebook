293660d4baa4896ee5b83203b7880e4e2398f1aa
Adrian Martinez <n.bicalcarata@gmail.com>
2018-04-29 20:42:10 -0500

    Migrate to vue components WIP

---
+++ resources/assets/js/components/Phonebook.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<script>
+export default {
+  created () {
+
+  },
+  mounted () {
+
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
                 font-family: 'Quicksand', sans-serif;
                 font-weight: 100;
                 height: 100vh;
-                margin: 0;
             }
 
         </style>
@@ -93,5 +92,7 @@
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+      <script>
+      </script>
     </body>
 </html>
